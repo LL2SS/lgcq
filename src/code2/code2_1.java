e573c356638efee83fe2b90ab13d6f7a817a4677
LL2SS <2546915436@qq.com>
2021-10-19 16:12:23 +0800

    hahaha

---
+++ src/code2/code2_1.java
@@ -0,0 +1,10 @@
+package code2;
+public class code2_1 {
+	public static void main(String[] args) {
+		double radius;
+		double area;
+		radius=20;
+		area=radius*radius*3.14159;
+		System.out.println("The area for the circle of radius"+radius+"is"+area);
+	}
+}
